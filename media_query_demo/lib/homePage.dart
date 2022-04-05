af313f3184c1c71f1b862d4f7364b52040d47aa3
yogitakumar <helloyogita@gmail.com>
2022-04-05 04:25:18 +0100

    MediaQuery device info

--- media_query_demo/lib/homePage.dart
+++ media_query_demo/lib/homePage.dart
@@ -3,20 +3,58 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final deviceInfo = MediaQuery.of(context);
     final screenHeight = deviceInfo.size.height;
     final screenWidth = deviceInfo.size.width;
-
     return Scaffold(
         appBar: AppBar(
           title: Text("Responsive App - MediaQuery"),
         ),
         body: deviceInfo.orientation == Orientation.portrait
-            ? Container(
-                height: screenHeight * 0.5,
-                color: Colors.red,
+           ? Row(
+                children: [
+                  SizedBox(
+                    width: screenWidth * 0.5,
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Screen Orientation : ${deviceInfo.orientation}"),
+                      Text("Screen Height : ${screenHeight.toStringAsFixed(2)}"),
+                      Text("Screen Width : ${screenWidth.toStringAsFixed(2)}"),
+                      Text("Screen pixel ratio : ${deviceInfo.devicePixelRatio}"),
+                      Text("Screen brightness : ${deviceInfo.platformBrightness}"),
+                      Text("Text Scale Factor : ${deviceInfo.textScaleFactor}"),
+                      Text("Is this device use 24 hour format? : ${deviceInfo.alwaysUse24HourFormat}")
+                    ],
+                  ),
+                ],
               )
-            : Container(width: screenWidth * 0.5, color: Colors.green));
+            : Column(
+                children: [
+                  SizedBox(
+                    height: screenHeight * 0.5,
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Screen Orientation : ${deviceInfo.orientation}"),
+                      Text("Screen Height : ${screenHeight.toStringAsFixed(2)}"),
+                      Text("Screen Width : ${screenWidth.toStringAsFixed(2)}"),
+                      Text("Screen pixel ratio : ${deviceInfo.devicePixelRatio}"),
+                      Text("Screen brightness : ${deviceInfo.platformBrightness}"),
+                      Text("Text Scale Factor : ${deviceInfo.textScaleFactor}"),
+                      Text("Is this device use 24 hour format? : ${deviceInfo.alwaysUse24HourFormat}")
+                    ],
+                  ),
+                ],
+              )
+    );
   }
 }
